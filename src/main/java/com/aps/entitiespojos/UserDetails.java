d6339f2494ff0407dc4f150c535c68f8808c8aa9
Palash Sarkar <palashsarkar0007@gmail.com>
2017-12-11 00:16:24 +0530

    Update UserDetails.java
    Minor formatting ...

--- src/main/java/com/aps/entitiespojos/UserDetails.java
+++ src/main/java/com/aps/entitiespojos/UserDetails.java
@@ -36,57 +36,58 @@ import javax.persistence.CascadeType;
 @Table (name="USER_DETAILS")
 public class UserDetails implements Serializable
 {
-	 private static final long serialVersionUID = 1L;
+	 
+	private static final long serialVersionUID = 1L;
 	 
                @Embedded
-	 private Address ad;
+	        private Address ad;
 	 
                @Id
                @Column (name="USER_ID",unique=true,nullable=false)
-               private String usr_id;
+                private String usr_id;
 	 
                @Column (name="FIRST_NAME",length=50,nullable=false)
-               private String firstname;
+                private String firstname;
 	 
                @Column (name="LAST_NAME",length=50,nullable=false)
-               private String lastname;
+                private String lastname;
         
                @Column (name="EMAIL_ID",length=500,unique=true,nullable=false)
-               private String email_id;
+                private String email_id;
 	 
                @Column (name="MOB_NO",unique=true,nullable=false,columnDefinition="char(10)")
-               private String mob_no;
+                private String mob_no;
                
                @Column (name="PASSWORD",length=25,unique=true,nullable=false)
-               private String pass; 
+                private String pass; 
                
                @OneToMany(fetch=FetchType.EAGER,cascade=CascadeType.ALL)
                @JoinTable(name="USER_RATINGS_NORMAL_MAP",joinColumns=@JoinColumn(name="UID"),
                           inverseJoinColumns=@JoinColumn(name="RATING_ID")
-                          )
-               private Collection<UserRatings> ur;
+                         )
+                private Collection<UserRatings> ur;
 	 
-	 public UserDetails()
-	 {
-	     ur=new ArrayList <UserRatings> ();
+	     public UserDetails()
+	     {
+	           ur=new ArrayList <UserRatings> ();
                    ad=new Address();
                    usr_id="";
-	     firstname="";
-	     lastname="";
-	     email_id="";
-	     mob_no="";
+	           firstname="";
+	           lastname="";
+	           email_id="";
+	           mob_no="";
                    pass="";
-	 }
+	     }
            
-            public void setUr(Collection <UserRatings> ur)
-            {
-                  this.ur=ur;
-            }
+             public void setUr(Collection <UserRatings> ur)
+             {
+                   this.ur=ur;
+             }
             
-            public Collection <UserRatings> getUr()
-            {
-                 return (ur);
-            }
+             public Collection <UserRatings> getUr()
+             {
+                   return (ur);
+             }
            
              public Address getAddress()
              {
@@ -98,59 +99,59 @@ public class UserDetails implements Serializable
                    this.ad=ad;
              }
 
-	public String getUsr_id() 
-	{
-		return (usr_id);
-	}
+	     public String getUsr_id() 
+	     {
+		   return (usr_id);
+	     }
 
-	public void setUsr_id(String usr_id) 
-	{
-		this.usr_id = usr_id;
-	}
+	     public void setUsr_id(String usr_id) 
+	     {
+		   this.usr_id = usr_id;
+	     }
 
-	public String getFirstname() 
-	{
-		return (firstname);
-	}
+	     public String getFirstname() 
+	     {
+		   return (firstname);
+	     }
 
-	public void setFirstname(String firstname) 
-	{
-		this.firstname = firstname;
-	}
+	     public void setFirstname(String firstname) 
+	     {
+		   this.firstname = firstname;
+	     }
 
-	public String getLastname() 
-	{
-		return (lastname);
-	}
+	     public String getLastname() 
+	     {
+		   return (lastname);
+	     }
 
-	public void setLastname(String lastname) 
-	{
-		this.lastname = lastname;
-	}
+	     public void setLastname(String lastname) 
+	     {
+		   this.lastname = lastname;
+	     }
 
-	public String getEmail_id() 
-	{
-		return (email_id);
-	}
+	     public String getEmail_id() 
+	     {
+		   return (email_id);
+	     }
 
-	public void setEmail_id(String email_id) 
-	{
-		this.email_id = email_id;
-	}
+	     public void setEmail_id(String email_id) 
+	     {
+		   this.email_id = email_id;
+	     }
 
-	public String getMob_no() 
-	{
-		return (mob_no);
-	}
+	     public String getMob_no() 
+	     {
+		   return (mob_no);
+	     }
 
-	public void setMob_no(String mob_no) 
-	{
-		this.mob_no = mob_no;
-	}
+	     public void setMob_no(String mob_no) 
+	     {
+		   this.mob_no = mob_no;
+	     }
           
              public void setPass(String password)
              {
-                  this.pass = password;
+                   this.pass = password;
              }
              
              public String getPass()
